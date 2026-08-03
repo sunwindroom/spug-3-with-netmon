9da25198beed33608c55e06f1e0b4f1bcac2fdb4
sunwind <sunwind@noreply.gitcode.com>
2026-08-03 15:43:02 +0800

    清理项目：仅保留源代码和必要文档，删除工具脚本/测试/压缩包/IDE缓存等

--- test_user.py
+++
@@ -1,6 +0,0 @@
-from apps.account.models import User
-u = User.objects.filter(is_supper=True).first()
-if u:
-    print(f'id={u.id} username={u.username} token={u.access_token}')
-else:
-    print('No superuser found')
